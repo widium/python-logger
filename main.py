917da9d2d304f783ceba60c081d7632ff8c72e75
Widium <eddiebennacer@orange.fr>
2023-12-22 11:16:04 +0100

    V1: Remove file example and clean the main.py

--- main.py
+++ main.py
@@ -10,36 +10,6 @@
 #                                                                              #
 # **************************************************************************** #
 
-# from basic_function import get_document
-# from logger import AdvancedLogger
-
-# logger = AdvancedLogger(
-#     log_directory="logs"
-# )
-
-# with logger.logging_context() as logger:
-#     print("This message will be logged.")
-#     raise Exception("This is an exception")
-
-
-
-from basic_function import get_document
-from logger import AdvancedLogger
-
-logger = AdvancedLogger(
-    log_directory="logs"
-)
-
-with logger.logging_context():
-    logfile = logger.log_path
-    print("This message will be logged.")
-    print("This message have multiple lines\nThis is the second line\nThis is the third line")
-
-print(logfile)
-
-
-
-from basic_function import get_document
 from logger import AdvancedLogger
 
 logger = AdvancedLogger(
